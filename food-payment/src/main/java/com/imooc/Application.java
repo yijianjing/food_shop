514072bd96c6357b9ffda449b4bf2b07d822ff67
yinjianjun <yinjianjing@126.com>
2020-10-03 12:28:59 +0800

    first start all

---
+++ food-payment/src/main/java/com/imooc/Application.java
@@ -0,0 +1,21 @@
+package com.imooc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+//扫描 mybatis mapper 包路径
+@MapperScan(basePackages = "com.imooc.mapper")
+//扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages= {"com.imooc", "org.n3r.idworker"})
+//@EnableConfigurationProperties
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
