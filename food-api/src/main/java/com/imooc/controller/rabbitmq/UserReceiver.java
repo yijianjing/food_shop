32b3f5fc0f454a4bca714f9062ba54a1f2aa169c
殷建军 <yinjianjun@shinhofood.com>
2020-10-13 14:57:44 +0800

    rabbitmq

--- food-api/src/main/java/com/imooc/controller/rabbitmq/UserReceiver.java
+++ food-api/src/main/java/com/imooc/controller/rabbitmq/UserReceiver.java
@@ -1,12 +1,17 @@
 package com.imooc.controller.rabbitmq;
 
 import com.imooc.controller.rabbitmq.model.User;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = "user")
+@RabbitListener(bindings = @QueueBinding(
+        value=@Queue(value="quser",durable="false",autoDelete = "true"),
+        exchange = @Exchange(value="euser",type = ExchangeTypes.TOPIC),
+        key="r"
+            )
+       )
 public class UserReceiver {
 
     @RabbitHandler
